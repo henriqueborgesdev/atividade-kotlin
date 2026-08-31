204050f20f1cf6775aa360a99ca7cf62b34c9701
henriqueborgesdev <henriqueborgesprog@gmail.com>
2026-08-31 20:21:49 -0300

    Atualizando botao que estava com erro

--- app/src/main/java/com/aula/appsbasicos/ui/screens/exercicios/BoasVindasLoginScreen.kt
+++ app/src/main/java/com/aula/appsbasicos/ui/screens/exercicios/BoasVindasLoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -50,6 +51,12 @@ fun BoasVindasLoginScreen(
                 text = "Bem-vindo(a), $nome!",
                 style = MaterialTheme.typography.titleMedium,
             )
+
+            Button(
+                onClick = aoVoltar
+            ) {
+                Text("Sair")
+            }
         }
     }
 }
